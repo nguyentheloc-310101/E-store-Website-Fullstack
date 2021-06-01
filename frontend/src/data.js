38b9fd612705ad2e7b3c84014d4ded27ef8e7a8d
Lộc Nguyễn Thế <nguyentheloc310101@gmail.com>
2021-06-01 19:27:16 +0700

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Drew on smile',
+            category: 'Shirts',
+            image:'/images/JB.jpg',
+            price: 120,
+            brand: 'Drew',
+            rating: 4.5,
+            numReviews: 10,
+            desciption:'high quality product'
+        },
+
+        {
+            _id:'2',
+            name:'Adidas CRD',
+            category: 'Shirts',
+            image:'/images/addi.jpg',
+            price: 200,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            desciption:'high quality product'
+        },
+
+        {
+            _id:'3',
+            name:'Off_white Mid-Season',
+            category: 'Shirts',
+            image:'/images/off.jpg',
+            price: 250,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 15,
+            desciption:'high quality product'
+        },
+
+        {
+            _id:'4',
+            name:'Balenciaga Revenue',
+            category: 'Shirts',
+            image:'/images/off.jpg',
+            price: 120,
+            brand: 'Balenciaga',
+            rating: 4,
+            numReviews: 10,
+            desciption:'high quality product'
+        },
+
+        {
+            _id:'5',
+            name:'Givenchy kinychi',
+            category: 'Shirts',
+            image:'/images/JB.jpg',
+            price: 260,
+            brand: 'Givenhcy',
+            rating: 4.5,
+            numReviews: 12,
+            desciption:'high quality product'
+        },
+
+        {
+            _id:'6',
+            name:'Basic Stussy  2018',
+            category: 'Shirts',
+            image:'/images/addi.jpg',
+            price: 230,
+            brand: 'Stussy',
+            rating: 4.5,
+            numReviews: 20,
+            desciption:'high quality product'
+        },     
+    ],
+};
+export default data;
